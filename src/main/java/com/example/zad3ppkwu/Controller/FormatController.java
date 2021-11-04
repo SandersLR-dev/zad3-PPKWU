75f7156b00b355437b9d7ac63547accd093dc86f
Dominik Snopek <222572@edu.p.lodz.pl>
2021-11-04 14:07:47 +0100

    create controller for XML format

--- src/main/java/com/example/zad3ppkwu/Controller/FormatController.java
+++ src/main/java/com/example/zad3ppkwu/Controller/FormatController.java
@@ -2,6 +2,8 @@ package com.example.zad3ppkwu.Controller;
 
 import com.example.zad3ppkwu.RequestSender;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +20,25 @@ public class FormatController {
 
         RequestSender requestSender = new RequestSender();
 
-        String response= requestSender.send("http://localhost:8080",text);
+        String response = requestSender.send("http://localhost:8080", text);
 
         ObjectMapper mapper = new ObjectMapper();
-
-        return mapper.readValue(response,Map.class);
-
+        return mapper.readValue(response, Map.class);
     }
+
+    @GetMapping(value = "/XML/{text}", produces = {"application/xml", "text/xml"})
+    public String xmlInterpreter(@PathVariable String text) {
+
+        RequestSender requestSender = new RequestSender();
+
+        String response = requestSender.send("http://localhost:8080", text);
+
+        JSONObject jsonObject = new JSONObject(response);
+
+        String xml = "<?xml version=\"1.0\" encoding=\"ISO-8859-15\"?>\n<" + "root" + ">" + XML.toString(jsonObject) + "</" + "root" + ">";
+
+        return xml;
+    }
+
+
 }
